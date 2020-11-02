b85cabc90fba8e82f2bc56dab1b5824c67311fb3
Alfredo Di Forti <adf.palermo@gmail.com>
2020-11-02 17:15:05 +0100

    Esercizio 2 Completato

--- javascript/main.js
+++ javascript/main.js
@@ -1,59 +1,36 @@
-/* funzione per caricare javascript dopo l'html e il CSS */
 $(document).ready(function(){
+//Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
+var arraynomi = [ 'luigi', 'andrea','luca','antonio','gabriele'];
 
-//Creare un array di oggetti
-//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-var catalogo = [
-    {
-        nome: 'Bianchi',
-        peso: 6.7,
-    },
-    {
-        nome: 'Coppi',
-        peso: 6.8,
-    },
-    {
-        nome: 'Lombardo',
-        peso: 7.1,
-    },
-];
-// HANDBLEBARS
-// creo una variabile che fa riferimento al template da utilizzare
-var stargate = $('#biciclette-template').html();
-// riporto la funzione di hanblebars che mi aiuta a compilare e le attribuisco una var
-var template = Handlebars.compile(stargate);
+var nuovalista = [];
 
-//valore di dafault
-var bicileggera = catalogo[0];
-//iterazione e  verifica
-for (var i = 1 ; i < catalogo.length ; i++) {
-    if(bicileggera.peso > catalogo[i]) {
-        catalogo[i] = bicileggera; 
-    }
+//Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
+//Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array) compresa tra i due numeri inseriti dall'utente.
+
+var min = parseInt(prompt('inserisci l\'indice minimo'));
+var max = parseInt(prompt('inserisci\'indice massimo'));
+
+for (var i = 0; i < arraynomi.length; i++ ) {
+    if (min <= i && i  >= max ) {
+        nuovalista.push(arraynomi[i]);
+    } 
 }
-// console log bici più leggera
-console.log(bicileggera);
-
-
-// STAMPARE A SCHERMO I RISULTATI
-//preparazione placeholder
-var dati = {
-    nome: bicileggera.nome,
-    peso: bicileggera.peso,
-};
-
-var html = template(dati); 
-$('.container').append(html);  
-
-/* funzionamento di Handlblebars:
-1) creo una var che fa riferimento al template da utilizzare
-2)importo da handlebars il .compile al quale faccio compilare la mia variabile
-3) creo un oggetto al quale attribuisco le chiavi che voglio compilare 
-4)creo una variabile html che innestera i dati nel template
-5)mando la mia nuova variabile HTML con append nel container prescelto
-*/
+console.log(nuovalista);
 
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
